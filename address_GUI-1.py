9ec21c64a3ab89566b6e27d2f4a921e3c745c23a
Tochi <tochukwu.iroakazi@ocsbstudent.ca>
2017-10-27 14:39:12 -0400

    Address program

---
+++ address_GUI-1.py
@@ -0,0 +1,10 @@
+# Created by : Mr. Coxall
+# Created on : 12-sep-2017
+# Created for : ICS3U 
+# Daily Assignment - Unit0-02 
+# This program displays Address program,but in GUI
+
+import ui
+
+view = ui.load_view()
+view.present('sheet')
